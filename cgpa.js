b986e92484f72b484075ef2135973a1693334de4
Ayush Singh <59230661+ayu8@users.noreply.github.com>
2021-11-22 09:49:21 +0530

    Update cgpa.js
    Added CSS manipulation to JS and fixed maximum input length for 'Credits' input

--- cgpa.js
+++ cgpa.js
@@ -2,6 +2,11 @@ var nrows = 0;
 
 document.getElementById("gpa_cal").onclick = function() {
 
+    document.getElementById("mini-container").style.display = "block";
+    document.getElementById("main-table").style.display = "none";
+    document.getElementById("result-show-div").style.display = "none";
+    document.getElementById("submit_button").style.display = "none";
+
     var tableElement = document.getElementById("use_js");
     tableElement.innerHTML = "";
     document.getElementById("show_result").innerHTML = "";
@@ -11,8 +16,11 @@ document.getElementById("gpa_cal").onclick = function() {
 
     document.getElementById("submit").onclick = function() {
 
+        document.getElementById("main-table").style.display = "block";
+        document.getElementById("submit_button").style.display = "inline";
+
         document.getElementById("show_result").innerHTML = "";
-        document.getElementById("table_heading").innerHTML = "<th>Course</th> <th>Credits</th> <th>Grades</th>";
+        document.getElementById("table_heading").innerHTML = "<th>Course</th> <th>Credits</th> <th>Grade Points</th>";
         var selectField = document.getElementById("courses_drop_down");
         var selected = selectField.options.selectedIndex;
         var selectedValue = selectField.options[selected];
@@ -21,13 +29,14 @@ document.getElementById("gpa_cal").onclick = function() {
         nrows = a;
         tableElement.innerHTML = "";
         for (var i=0; i<a; i++) {
-            tableElement.innerHTML += "<td> <input type='text' placeholder='Course " + (i+1) + "'> </td> <td> <input class='cred' max='6' required type='number'> </td> <td> <select class='grad'> <option value=''> ---Select Grade--- </option> <option value='10'>S</option> <option value='9'>A</option> <option value='8'>B</option> <option value='7'>C</option> <option value='6'>D</option> <option value='5'>E</option> <option value='0'> N/F </option>  </td>";
+            tableElement.innerHTML += "<td> <input type='text' placeholder='Course " + (i+1) + "'> </td> <td> <input class='cred' onKeyDown='if(this.value.length==1 && event.keyCode!=8) return false;' required type='number'> </td> <td> <select class='grad'> <option value=''> ---Select Grade--- </option> <option value='10'>S</option> <option value='9'>A</option> <option value='8'>B</option> <option value='7'>C</option> <option value='6'>D</option> <option value='5'>E</option> <option value='0'> N/F </option> </select> </td>";
         }
 
         document.getElementById("submit_button").innerHTML = "<input type='submit' class='buttons' value='Calculate'>";
     }
 
-    document.getElementById("submit_button").onclick = function() { 
+    document.getElementById("submit_button").onclick = function() {
+
         var gradePoints = 0;
         var creditsTotal = 0;
         
@@ -40,12 +49,18 @@ document.getElementById("gpa_cal").onclick = function() {
         }
 
         var res = gradePoints/creditsTotal;
+        document.getElementById("result-show-div").style.display = "block";
         document.getElementById("show_result").innerHTML = "Your GPA comes out to be: " + res.toFixed(2);
-        alert("Your GPA comes out to be: " + res.toFixed(2));
+        // alert("Your GPA comes out to be: " + res.toFixed(2));
     }
 }
 
 document.getElementById("cgpa_cal").onclick = function() {
+
+    document.getElementById("mini-container").style.display = "block";
+    document.getElementById("main-table").style.display = "none";
+    document.getElementById("result-show-div").style.display = "none";
+    document.getElementById("submit_button").style.display = "none";
 
     var tableElement = document.getElementById("use_js");
     tableElement.innerHTML = "";
@@ -55,6 +70,9 @@ document.getElementById("cgpa_cal").onclick = function() {
     document.getElementById("choose_dropdown").innerHTML = "<p>Enter number of semesters: <select id='courses_drop_down'> <option value='2'>2</option> <option value='3'>3</option> <option value='4'>4</option> <option value='5'>5</option> <option value='6'>6</option> <option value='7'>7</option> <option value='8'>8</option> </select> <input id='submit' class='buttons' type='submit'> </p>";
 
     document.getElementById("submit").onclick = function() {
+
+        document.getElementById("main-table").style.display = "block";
+        document.getElementById("submit_button").style.display = "inline";
 
         document.getElementById("show_result").innerHTML = "";
 
@@ -67,7 +85,7 @@ document.getElementById("cgpa_cal").onclick = function() {
         nrows = a;
         tableElement.innerHTML = "";
         for (var i=0; i<a; i++) {
-            tableElement.innerHTML += "<td> <input type='text' placeholder='Semester " + (i+1) + "'> </td><td> <input class='cred' max='27' required type='number'> </td><td> <input type='number' max='10' required class='grad'> </td>";
+            tableElement.innerHTML += "<td> <input type='text' placeholder='Semester " + (i+1) + "'> </td><td> <input class='cred' required type='number' onKeyDown='if(this.value.length==2 && event.keyCode!=8) return false;'> </td><td> <input type='number' max='10' required class='grad'> </td>";
         }
 
         document.getElementById("submit_button").innerHTML = "<input type='submit'; class='buttons'  value='Calculate'>";
@@ -86,7 +104,9 @@ document.getElementById("cgpa_cal").onclick = function() {
         }
 
         var res = gradePoints/creditsTotal;
+        document.getElementById("result-show-div").style.display = "block";
         document.getElementById("show_result").innerHTML = "Your CGPA comes out to be: " + res.toFixed(2);
-        alert("Your CGPA comes out to be: " + res.toFixed(2));
+        // alert("Your CGPA comes out to be: " + res.toFixed(2));
     }
 }
+
